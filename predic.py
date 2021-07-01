87fa44a6f73f95cce419880d026e5f2615d65c7f
Kidchris <christianjosaphat0@gmail.com>
2021-07-01 14:51:25 +0000

    update

--- predic.py
+++ predic.py
@@ -1,17 +1,13 @@
 import cv2 as cv
-import numpy as np
-import warnings
 
 
 live = cv.VideoCapture(0)
 live.set(3, 850)
 live.set(4, 750)
-# HAAR_FACE = cv.CascadeClassifier("./data/haarcascade_frontalface_alt2.xml")
 
 HAAR_FACE = cv.CascadeClassifier("./data/haarcascade_frontalface_alt2.xml")
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read("recognizer.yml")
-# face = HAAR_FACE.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=2)
 
 
 peoples= ['TonyStark', 'chrisKid', 'elonMusk']
@@ -27,14 +23,13 @@ while True:
         roi_c = frame[y:y+h, x:x+w]
 
         label, confidence = face_recognizer.predict(roi)
+        text = f"{peoples[label]}({confidence :.2f}%)"
 
         cv.rectangle(frame, (x, y), (x+w, y+h), (29, 90, 250), thickness = 2)
-        cv.putText(frame, str(peoples[label]), (x,y), cv.FONT_HERSHEY_SIMPLEX, .6, (255, 55, 35), 1, cv.LINE_AA)
+        cv.putText(frame, text, (x+2,y-4), cv.FONT_HERSHEY_SIMPLEX, .6, (255, 255, 255), 1, cv.LINE_AA)
         
         cv.imwrite("./chris_face.png", roi)
         cv.imwrite("./chris_face_colored.png", roi_c)
-
-        # print(peoples[label])
 
     cv.imshow("live Video detected faces ", frame)
     if cv.waitKey(20) & 0xFF==ord('q'):
